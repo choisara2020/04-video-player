e0fb2882769a90725f6f4bd127f2f0fe99ff1e01
Sara C <choisara2010@gmail.com>
2023-04-26 15:31:52 -0400

    second commit

---
+++ src/Video.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./styles.css";
+
+function Video() {
+  return (
+    <div>
+      <video controls autostart autoPlay muted />
+    </div>
+  );
+}
+export default Video;
